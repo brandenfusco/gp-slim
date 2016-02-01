4dd7d9cca27e79fc599b6e5a22687a7c3cfb6b64
Jamie Smith <jamie.smith@blueacorn.com>
2016-02-01 14:24:32 -0500

    added some simple documentation on the plugin, change the names of the plugin to all match because I am dumb

--- webroot/skin/frontend/blueacorn/gp/js/blueacorn/blueacorn-plugin-change-text.js
+++ webroot/skin/frontend/blueacorn/gp/js/blueacorn/blueacorn-plugin-change-text.js
@@ -1,8 +1,31 @@
 /**
- * @package     Blueacorn\\ChangeText
+ * @package     Blueacorn\\TextChange
  * @version     1.0
  * @author      Blue Acorn <code@blueacorn.com>
  * @copyright   Copyright © 2015 Blue Acorn.
+ *
+ * jQuery plugin for when we need to change the text of
+ * an HTML element based on some set of stipulations.
+ *
+ * ##### EXAMPLES
+ *
+ * For instance, if we need to edit the text of the .page-title element
+ * from the current text (Sample) to something else (New Page Title) we
+ * can use the following method to allow that.
+ *
+ * // Initiate the plugin on element
+ * var $pageTitle = $j('.page-title > h1').textChange();
+ *
+ * // On button click, change the text using custom jQuery method
+ * $('button').on('click', function(){
+ *      $pageTitle.updateText("New Page Title");
+ * });
+ *
+ * If you by chance want to change the element back to the original
+ * text, you can simply pass in nothing into the changeText() method.
+ *
+ * $pageTitle.updateText();
+ *
  */
 
 ;(function ($, window, document, undefined) {
@@ -10,7 +33,7 @@
     var pluginName = "textChange";
 
     // The actual plugin constructor
-    function ChangeText(element) {
+    function TextChange(element) {
         this.$element = $(element);
         this.init();
     }
@@ -22,7 +45,7 @@
      * @param element
      *  jQuery element to act upon
      */
-    ChangeText.prototype = {
+    TextChange.prototype = {
 
         init: function() {
             this.addDefaultPropToElement();
@@ -41,7 +64,7 @@
     };
 
     $.fn[pluginName] = function() {
-        return new ChangeText(this);
+        return new TextChange(this);
     };
 
 })(jQuery, window, document);
