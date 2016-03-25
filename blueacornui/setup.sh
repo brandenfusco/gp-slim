0eed7728a54cb9e5c0d21e66e925724eedd4c22d
Greg Harvell <greg@blueacorn.com>
2016-03-25 10:34:31 -0400

    Version Bump

--- blueacornui/setup.sh
+++ blueacornui/setup.sh
@@ -2,7 +2,7 @@
 
 #/**
 # * @package     BlueAcorn/GreenPistachio
-# * @version     4.1.0
+# * @version     
 # * @author      Blue Acorn, Inc. <code@blueacorn.com>
 # * @copyright   Copyright © 2016 Blue Acorn, Inc.
 # */
