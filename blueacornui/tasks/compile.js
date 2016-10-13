a5f5839f4abdbab83c996dc601a3a3891d82a675
Greg Harvell <greg@blueacorn.com>
2016-10-13 11:55:47 -0400

    Drying out some tears with a better task system for Jamie.

--- blueacornui/tasks/compile.js
+++ blueacornui/tasks/compile.js
@@ -1,6 +1,6 @@
 /**
 * @package     BlueAcorn/GreenPistachio
-* @version     4.5.0
+* @version     4.4.0
 * @author      Blue Acorn, Inc. <code@blueacorn.com>
 * @copyright   Copyright © 2016 Blue Acorn, Inc.
 */
@@ -11,40 +11,17 @@ module.exports = function(grunt) {
     var _ = require('underscore'),
         path = require('path'),
         themes = require('../configs/themes'),
-        configDir = '../configs';
+        configDir = '../configs',
+        helper = require('./helper');
 
     grunt.registerTask('compile', 'Theme Compilation', function() {
         if(arguments[0]) {
-            grunt.task.run('clean:' + arguments[0] + 'Prepare');
-            grunt.task.run('sass:' + arguments[0] + 'Dev');
-            grunt.task.run('postcss:' + arguments[0] + 'Dev');
-            grunt.task.run('jshint:' + arguments[0]);
-            if(arguments[0].jsdirs.length > 0) {
-                _.each(arguments[0].jsdirs, function(jsdir){
-                    grunt.task.run('copy:' + arguments[0] + 'BuildFallback' + jsdir);
-                    grunt.task.run('copy:' + arguments[0] + 'BuildTheme' + jsdir);
-                });
-            }
-            grunt.task.run('uglify:' + arguments[0] + 'Dev');
-            grunt.task.run('concurrent:' + arguments[0] + 'Images');
-            grunt.task.run('clean:' + arguments[0] + 'Prepare');
+            helper.runTasks(arguments[0], 'compile', grunt);
             grunt.task.run('shell:cache');
         }else{
             _.each(themes, function(theme, name){
                 if(theme.grunt) {
-                    grunt.task.run('clean:' + name + 'Prepare');
-                    grunt.task.run('sass:' + name + 'Dev');
-                    grunt.task.run('postcss:' + name + 'Dev');
-                    grunt.task.run('jshint:' + name);
-                    if(theme.jsdirs.length > 0) {
-                        _.each(theme.jsdirs, function(jsdir){
-                            grunt.task.run('copy:' + name + 'BuildFallback' + jsdir);
-                            grunt.task.run('copy:' + name + 'BuildTheme' + jsdir);
-                        });
-                    }
-                    grunt.task.run('uglify:' + name + 'Dev');
-                    grunt.task.run('concurrent:' + name + 'Images');
-                    grunt.task.run('clean:' + name + 'Prepare');
+                    helper.runTasks(name, 'compile', grunt);
                 }
             });
             grunt.task.run('shell:cache');
